71fbf384ecc6e5e298d8c7450912d019bb677d63
BrunaMartins <brunet_nunes123@hotmail.com>
2023-12-18 22:03:51 -0300

    Alteração e inclusão de novas moedas

--- scripts.js
+++ scripts.js
@@ -1,89 +1,98 @@
-/*   ////console.log("funcionou?")//////   Para testar se está conectado com meu html*/
 
-const convertButton = document.querySelector(".convert-button")
+const convertButton = document.querySelector(".convert-button") /*mapeando meu botão de conversão*/
 const currencySelect = document.querySelector(".currency-select")
 
-function convertValues() {  /*FUNÇÃO: quando eu chamar essa funçaõ quero que converta esses valores*/
-    /*console.log("Funcionou") console.log para eu ter certeza que estou chamando a minha função*/
 
+function convertValues() { /*funçaõ*/
+    const inputCurrencyValue = document.querySelector(".input-currency").value /*capturar o valor do meu input*/
+    const currencyValueToConvert = document.querySelector(".currency-value")
+    const currencyValueConverted = document.querySelector(".currency-converted")
 
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-
-    const currencyValueToConvert = document.querySelector(".currency-value")     /*Valor em Real*/
-    const currencyValueConverted = document.querySelector(".currency-converted") /*Valor  convertido*/
-
-
-console.log(currencySelect.value)
-
-    const dolarToday = 5.2
-    const euroToday = 6.3
 
 
 
-if(currencySelect.value == "dolar"){ // Se o select estiver selecionado o valor de dólar, entre aqui*/
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency", currency: "USD"
-    }).format(inputCurrencyValue / dolarToday)
+    const dolarToday = 5.2
+    const euroToday = 6.2
+    const libraToday = 6.5
+    const realtoday = 1.0
 
 
-}
-
-if(currencySelect.value == "euro"){// Se o select estiver selecionado o valor de euro, entre aqui*/
-currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE",{
-style: "currency", currency: "EUR"
-}).format(inputCurrencyValue/euroToday)
+    
 
 
-}
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
 
-    /*innerHTML - trocar texto*/
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", { /*formatação dos números em Real*/
-        style: "currency", currency: "BRL"
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+
+
+    }
+
+    if (currencySelect.value == "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday)
+
+    }
+
+    if (currencySelect.value == "bitcoin") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "BTC"
+        })
+
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", { /*lingua e localidade*/
+        style: "currency",
+        currency: "BRL" /*sigla da moeda*/
     }).format(inputCurrencyValue)
 
 
- 
 
-
-    /*currencyValueConverted.innerHTML = convertedValue
-
-    console.log(convertedValue)*/
 
 }
 
 
-
-function changeCurrency (){
-const currencyName = document.getElementById("currency-name")
-currencyImg = document.querySelector(".currency-img")
-
+function changeCurrency() {/*toda vez que trocar de moeda eu chamo essa função*/
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
 
 
-if(currencySelect.value == "dolar"){
-currencyName.innerHTML = "Dólar americano"
-currencyImg.src = "./assets/dolar.png"
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dolar Americano"
+        currencyImage.src = "./assests/dolar.png"
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/euro.png"
+    }
+
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "./assets/libra.png"
+    }
+
+    if (currencySelect.value == "real") {
+        currencyName.innerHTML = "Real"
+        currencyImage.src = "./assets/real.png"
+    }
+
 
 }
-
-if(currencySelect.value == "euro"){
-currencyName.innerHTML = "Euro"
-currencyImg.src = "./assets/euro.png"
-
-}
-
-convertValues()
-
-}
-
-
-
-
-
-
-currencySelect.addEventListener("change",changeCurrency )
-
-convertButton.addEventListener("click", convertValues) /*  OUVINTE(addEventListenet) - MEU BOTAO (convertButton)- CLICK (qdo ele é clicado) - OBJETIVO(convertValues) toda vez que for clicado ele converte os valolres
-*/
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues) /*ouvinte do meu botão quando ele é clicado---toda vez que ele for clicado ele chama a função acima*/
 
 
 
